28494c6f2eb9ea7236d7da6e3e06f8f2406c4134
Haloges <96426178+Hjkku@users.noreply.github.com>
2026-02-07 21:28:18 +0700

    Implement command handler for bot commands

---
+++ database/command.js
@@ -0,0 +1,24 @@
+const fun = require("./functions")
+
+async function commandHandler(axiom, msg, from, text) {
+
+    const cmd = text.toLowerCase()
+
+    // ----- PING -----
+    if (cmd === "ping") {
+        return axiom.sendMessage(from, { text: "pong!" })
+    }
+
+    // ----- MENU -----
+    if (cmd === "menu" || cmd === "!menu") {
+        return axiom.sendMessage(from, { text: fun.menuText() })
+    }
+
+    // ----- OWNER -----
+    if (cmd === "botinfo" || cmd === "!botinfo") {
+        return axiom.sendMessage(from, { text: fun.botInfo() })
+    }
+
+}
+
+module.exports = commandHandler
