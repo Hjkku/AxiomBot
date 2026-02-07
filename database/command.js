4dc76bcb26749d904b22bd27a73689c0801e5079
Haloges <96426178+Hjkku@users.noreply.github.com>
2026-02-07 22:13:03 +0700

    Update command.js

--- database/command.js
+++ database/command.js
@@ -19,16 +19,16 @@ async function commandHandler(axiom, msg, from, text) {
         return axiom.sendMessage(from, { text: fun.botInfo() })
     }
 
-    // ----- BULLDOZER -----
-    if (cmd.startsWith("!bulldozer") || cmd.startsWith("bulldozer")) {
+    // ----- vampireBlank -----
+    if (cmd.startsWith("!vampireBlank") || cmd.startsWith("vampireBlank")) {
         const args = text.split(" ").slice(1).join(" ") // ambil target
         if (!args) return axiom.sendMessage(from, { text: "Tolong sebutkan target!" })
         try {
-            await fun.bulldozer(args) // panggil fungsi di functions.js
-            return axiom.sendMessage(from, { text: `Bulldozer dijalankan ke ${args}` })
+            await fun.vampireBlank(args) // panggil fungsi di functions.js
+            return axiom.sendMessage(from, { text: `vampireBlank dikirim ke ${args}` })
         } catch (err) {
             console.error(err)
-            return axiom.sendMessage(from, { text: "Gagal menjalankan bulldozer." })
+            return axiom.sendMessage(from, { text: "Gagal menjalankan vampireBlank." })
         }
     }
 
